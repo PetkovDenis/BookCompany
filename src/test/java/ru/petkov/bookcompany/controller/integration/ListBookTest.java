d523c7eb8ca313df99743af487da2dd5dfd23bd1
DenisPetkov <fini.denif.99@mail.ru>
2024-07-15 20:41:59 +1000

    Target files removed

--- src/test/java/ru/petkov/bookcompany/controller/integration/ListBookTest.java
+++ src/test/java/ru/petkov/bookcompany/controller/integration/ListBookTest.java
@@ -4,19 +4,23 @@ package ru.petkov.bookcompany.controller.integration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import ru.petkov.bookcompany.entity.Book;
 import ru.petkov.bookcompany.entity.Client;
 import ru.petkov.bookcompany.service.book.BookService;
+import ru.petkov.bookcompany.service.book.BookServiceImpl;
 import ru.petkov.bookcompany.service.client.ClientService;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,14 +32,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ListBookTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private  MockMvc mockMvc;
     @Autowired
-    private BookService bookService;
+    private  BookService bookService;
     @Autowired
-    private ClientService clientService;
+    private  ClientService clientService;
+
+
 
     @BeforeEach
     public void init() {
+
 
         Book book = new Book();
         book.setTitle("title1");
